898a8cb8541c5c801fc1c68ff90d56f8c38c0dce
adityau <adityau.fns@gmail.com>
2024-12-27 16:49:40 +0530

    new-projects

---
+++ Hoverboard/script.js
@@ -0,0 +1,25 @@
+const conatiner = document.querySelector('.container');
+const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange'];
+const SQUARES = 500;
+
+for(let i=0;i<SQUARES;i++){
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.addEventListener('mouseover',()=>setColor(square));
+    square.addEventListener('mouseout',()=>removeColor(square));
+
+    conatiner.appendChild(square);
+}
+
+function setColor(element){
+    const color = getRandomColor();
+    element.style.backgroundColor = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+function removeColor(element){
+    element.style.backgroundColor = '#1d1d1d';
+    element.style.boxShadow = '0 0 2px #000';
+}
+function getRandomColor(){
+    return colors[Math.floor(Math.random()*colors.length)];
+}
